63c555d6316bb9f9114a036a98b4b8b20142a5f6
Денис <denis070619870@rambler.ru>
2022-08-10 17:35:16 +0300

    Example002_FirstNumberSquareSecond

---
+++ Example002_FirstNumberSquareSecond/Program.cs
@@ -0,0 +1,19 @@
+﻿// По двум заданным числам проверять является ли одно квадратом другого
+
+Console.WriteLine("Введите первое число: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int b = int.Parse(Console.ReadLine());
+
+if (a * a == b)
+{
+    Console.WriteLine("число " + a + " "+ "является квадратом числа: " + b);
+}
+else if (b * b == a)
+{
+    Console.WriteLine("число " + b + " "+ "является квадратом числа: " + a);
+}
+else
+{
+    Console.WriteLine("числа не являются квадратом друг друга");
+}
