bd7c5e2ec0a6285a85fdccb184b139a46a0afa95
Денис <denis070619870@rambler.ru>
2022-08-10 16:48:33 +0300

    Example001_FirstNumberSquareSecond

--- Example001_FirstNumberSquareSecond/Program.cs
+++ Example001_FirstNumberSquareSecond/Program.cs
@@ -1,15 +1,38 @@
-﻿// По двум заданным числам проверять является ли первое квадратом второго
+﻿// Дано число обозначающее день недели. Выяснить является номер дня недели выходным
 
-Console.WriteLine("Введите число, которое будет являться квадратом второго: ");
-int a = int.Parse(Console.ReadLine());
-Console.WriteLine("Введите второе число: ");
-int b = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите номер дня недели: ");
+int numberDay = int.Parse(Console.ReadLine());
 
-int square = a * a;
-
-if ( square == b)
+while (numberDay > 5)
 {
-    Console.WriteLine("Число: " + a + " " + "является квадратом числа: " + b);
+    Console.WriteLine("Этот день выходной");
+    break;
 }
-else
-Console.WriteLine("Число: " + a + " " + "не является квадратом числа: " + b);
+   if (numberDay == 1)
+    {
+        Console.WriteLine("Понедельник");
+    }
+    else if (numberDay == 2)
+    {
+        Console.WriteLine("Вторник");
+    }
+    else if (numberDay == 3)
+    {
+        Console.WriteLine("Среда");
+    }
+    else if (numberDay == 4)
+    {
+        Console.WriteLine("Четверг");
+    }
+    else if (numberDay == 5)
+    {
+        Console.WriteLine("Пятница");
+    }
+    else if (numberDay == 6)
+    {
+        Console.WriteLine("Суббота");
+    }
+    else if (numberDay == 7)
+    {
+        Console.WriteLine("Воскресенье");
+    }
