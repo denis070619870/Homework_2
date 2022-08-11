d7fabe20176361d717e1b1ee3cf7dc5f7ae4e6a7
Денис <denis070619870@rambler.ru>
2022-08-11 15:56:34 +0300

    Example004_Distance

---
+++ Example004_Distance/Program.cs
@@ -0,0 +1,8 @@
+﻿// Найти расстояние между точками в пространстве 2D/3D
+
+
+int a = new Random().Next(10, 21);
+Console.Write(a);
+
+Console.WriteLine("Ввеите число, на которое нужно проверить кратность выбранного числа: ");
+int b = int.Parse(Console.ReadLine());
