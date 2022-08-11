fe1841cae7583035d1c8a15fa40c1ee261d9384c
Денис <denis070619870@rambler.ru>
2022-08-11 18:09:51 +0300

    Example005_Cube

---
+++ Example005_Cube/Program.cs
@@ -0,0 +1,12 @@
+﻿// Найти кубы чисел от 1 до N
+
+Console.WriteLine("Введите число до которого нужно вывести кубы чисел:");
+int n = int.Parse(Console.ReadLine());
+
+int count = 1;
+
+while (count <= n)
+{
+    Console.WriteLine(count * count * count);
+    count = count + 1;
+}
