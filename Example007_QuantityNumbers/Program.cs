f2f1793ad48567de48deb52c35263b2fced66682
Денис <denis070619870@rambler.ru>
2022-08-12 10:49:13 +0300

    Example007_QuantityNumbers

---
+++ Example007_QuantityNumbers/Program.cs
@@ -0,0 +1,17 @@
+﻿// Определить количество цифр в числе
+
+//Console.WriteLine();
+Console.WriteLine("Введите число, чтобы определить из скольки цифр оно состоит: ");
+string n = Console.ReadLine();
+int num = Convert.ToInt32(n);
+int result = num;
+
+int count = 0;
+
+while (result > 0)
+{
+    result = result / 10;
+    count++;
+}
+Console.WriteLine($"В числе{num} имеется {count} чисел.");
+Console.WriteLine();
