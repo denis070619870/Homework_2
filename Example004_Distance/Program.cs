9fd95630448f4ca5c61830fe2d251cebe6478abd
Денис <denis070619870@rambler.ru>
2022-08-14 12:45:18 +0300

    Example004_Distance

--- Example004_Distance/Program.cs
+++ Example004_Distance/Program.cs
@@ -6,3 +6,53 @@ Console.Write(a);
 
 Console.WriteLine("Ввеите число, на которое нужно проверить кратность выбранного числа: ");
 int b = int.Parse(Console.ReadLine());
+
+d = sqrt((xb-xa)^2 + (yb-ya)^2+(zb-za)^2) // Формула расстояния
+
+
+
+
+int numPlan = 3;            // количество осей координат
+int min = -5;               // минимальное значение диапазона для выбора случайных чисел
+int max = 6;                // максимальное значение диапазона ...
+
+int[] PointCoords(int numPlan)                  
+{
+    int[] coords = new int[numPlan];
+    for (int i = 0; i < numPlan; i++)
+    {
+        coords[i] = new Random().Next(min, max);
+    }
+    return coords;
+}
+
+void PrintCoords(int[] array)
+{
+    Console.Write($"({array[0]}, ");
+    for (int i = 1; i < array.Length - 1; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+    Console.WriteLine($"{array[array.Length - 1]})");
+}
+
+double Distance(int[] arr1, int[] arr2)
+{
+    int sum = 0;
+    for (int i = 0; i < arr1.Length; i++)
+    {
+        sum = sum + (arr1[i] - arr2[i]) * (arr1[i] - arr2[i]);
+    }
+    double result = Math.Sqrt(sum);
+    return result;
+}
+
+int[] PointA = PointCoords(numPlan);
+int[] PointB = PointCoords(numPlan);
+
+Console.Write($"Координаты точки А: ");
+PrintCoords(PointA);
+Console.Write($"Координаты точки B: ");
+PrintCoords(PointB);
+
+Console.WriteLine($"Расстояние между точками А и В: {Distance(PointA, PointB)}");
