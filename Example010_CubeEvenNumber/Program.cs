61a796861a8e660d7975d95b0ffbfe3938e3f873
Денис <denis070619870@rambler.ru>
2022-08-14 11:30:13 +0300

    Example010_CubeEvenNumber

---
+++ Example010_CubeEvenNumber/Program.cs
@@ -0,0 +1,15 @@
+﻿// Показать кубы чисел, заканчивающихся на четную цифру
+
+Console.WriteLine("Введите число до которого нужно вывести кубы четных чисел:");
+int n = int.Parse(Console.ReadLine());
+
+int count = 1;
+
+while (count <= n)
+{
+    if (count % 2 == 0)
+    {
+        Console.WriteLine(count * count * count);
+    }
+    count = count + 1;
+}
