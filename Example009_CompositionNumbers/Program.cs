bbece555a46459e4e31de468042366abe0e83e66
Денис <denis070619870@rambler.ru>
2022-08-30 18:09:31 +0300

    Example009_CompositionNumbers

--- Example009_CompositionNumbers/Program.cs
+++ Example009_CompositionNumbers/Program.cs
@@ -1,19 +1,35 @@
 ﻿// Написать программу вычисления произведения чисел от 1 до N
 Console.WriteLine("Введите число, до которого нужно вывести произведения чисел: ");
-int n = int.Parse(Console.ReadLine());
+//int n = int.Parse(Console.ReadLine());
 
-int count = 1;
+//int count = 1;
 
-while (count <= n)
+//while (count <= n)
+//{
+//    for ( int i = 2; i <=n; i++)
+//    {
+//        for (int j =2; j <=n; j++)
+//        {
+//            Console.WriteLine($"{i} * {j} = {i * j} ");
+//        }
+//        Console.WriteLine();
+//    }
+//    break;
+//}
+
+
+string writeN = Console.ReadLine();
+int n = Convert.ToInt32(writeN);
+
+int NumberComposition(int x)
 {
-    for ( int i = 2; i <=n; i++)
+    int count = 1;
+    for (int i = 1; i <= x; i++)
     {
-        for (int j =2; j <=n; j++)
-        {
-            Console.WriteLine($"{i} * {j} = {i * j} ");
-        }
-        Console.WriteLine();
+        count = count * i;
     }
-    break;
+    return count;
 }
 
+Console.WriteLine($"Произведение чисел от 1 до {n} = {NumberComposition(n)}");
+Console.WriteLine();
