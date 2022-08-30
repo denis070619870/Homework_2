e8f42201c9600fa07e0922acc67da5e2eed0ee78
Денис <denis070619870@rambler.ru>
2022-08-30 18:19:16 +0300

    Example006_Sum

--- Example006_Sum/Program.cs
+++ Example006_Sum/Program.cs
@@ -2,13 +2,17 @@
 
 Console.WriteLine("Введите число до которого нужно посчитать сумму всех чисел:");
 int A = int.Parse(Console.ReadLine());
+Console.WriteLine();
 
 int count = 1;
 int sum = 0;
+
 
 while (count <= A)
 {
     Console.Write(sum = sum + count);
     count = count + 1;
 }
+Console.WriteLine();
 Console.WriteLine($"Сумма чисел от 1 до {A} составляет {sum}");
+Console.WriteLine();
